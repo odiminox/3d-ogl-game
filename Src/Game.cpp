afef813a9805c80f00b6777df1c9bf8ffc2ba0df
Simon Jordan <simon@thepolygonloft.com>
2022-02-06 12:53:37 +0000

    vertex and fragment shader init with placeholder vertex data

--- Src/Game.cpp
+++ Src/Game.cpp
@@ -38,10 +38,9 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
   glViewport(0, 0, width, height);
 }
 
-int init()
-{
-  std::cout << " INIT! " << std::endl;
 
+int render_init()
+{
   std::cout << "Graphics Booting up..." << std::endl;
 
   glfwInit();
@@ -66,6 +65,103 @@ int init()
   glViewport(0, 0, 800, 600);
   glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+  float vertices[] = {
+   -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.0f,  0.5f, 0.0f
+  };
+
+  std::cout << "compiling vertex shader..." << std::endl;
+
+  const char* vertex_shader_code = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+
+  unsigned int VBO;
+  glGenBuffers(1, &VBO);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  unsigned int vertex_shader;
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+
+  glShaderSource(vertex_shader, 1, &vertex_shader_code, nullptr);
+  glCompileShader(vertex_shader);
+
+  int  success;
+  char infoLog[512];
+  glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+
+  if (!success)
+  {
+    glGetShaderInfoLog(vertex_shader, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+    return -1;
+  }
+
+  std::cout << "compiling fragment shader..." << std::endl;
+
+  const char* fragment_shader_code = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\0";
+
+
+  unsigned int fragment_shader;
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_code, nullptr);
+  glCompileShader(fragment_shader);
+
+  glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+
+  if (!success)
+  {
+    glGetShaderInfoLog(fragment_shader, 512, nullptr, infoLog);
+    std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+
+    return -1;
+  }
+
+  std::cout << "Creating shader program" << std::endl;
+
+  unsigned int shader_program;
+  shader_program = glCreateProgram();
+  glAttachShader(shader_program, vertex_shader);
+  glAttachShader(shader_program, fragment_shader);
+  glLinkProgram(shader_program);
+
+  glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+
+  if (!success)
+  {
+    glGetProgramInfoLog(shader_program, 512, nullptr, infoLog);
+    std::cout << "ERROR:SHADER_PROGRAM::CREATION_FAILED\n" << infoLog << std::endl;
+
+    return -1;
+  }
+
+  glUseProgram(shader_program);
+  glDeleteShader(vertex_shader);
+  glDeleteShader(fragment_shader);
+
+  std::cout << "Graphics stage booted up" << std::endl;
+}
+
+int init()
+{
+  std::cout << " INIT! " << std::endl;
+
+  if (!render_init())
+  {
+    return -1;
+  }
+
   std::cout << "initialising world entities" << std::endl;
   entities.push_back(a);
   entities.push_back(b);
@@ -86,7 +182,7 @@ void quit()
   delete c;
 }
 
-void world_update(double delta_time)
+void world_update(int delta_time)
 {
   for (auto entity : entities)
   {
@@ -94,12 +190,14 @@ void world_update(double delta_time)
   }
 }
 
+
 void world_render()
 {
   glfwSwapBuffers(window);
   glfwPollEvents();
   glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
   glClear(GL_COLOR_BUFFER_BIT);
+
 }
 
 int simulation_time = 0;
